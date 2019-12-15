429ac94d17056883414ae0f30764be533204979d
Elias Norrby <elias.norrby@gmail.com>
2019-12-15 16:27:07 +0100

    L8: Operators

--- RustSample/hello_world/src/main.rs
+++ RustSample/hello_world/src/main.rs
@@ -1,6 +1,6 @@
 use std::mem;
 
-fn main() {
+fn fundamental_data_types() {
     // unsigned
     let a: u8 = 123;
     println!("a = {}", a);
@@ -38,4 +38,42 @@ fn main() {
 
     let f = 4 > 0;
     println!("f = {}, size = {} bytes", f, mem::size_of_val(&f));
+}
+
+fn operators() {
+    // arithmetic
+    let mut a = 2 + 3 * 4;
+    println!("{}", a);
+    a = a + 1;
+    a -= 2;
+
+    println!("remainder of {} / {} = {}", a, 3, (a % 3));
+
+    let a_cubed = i32::pow(a, 3);
+    println!("{} cubed is {}", a, a_cubed);
+
+    let b = 2.5;
+    let b_cubed = f64::powi(b, 3);
+    let b_to_pi = f64::powf(b, std::f64::consts::PI);
+    println!("{} cubed = {}, {}^pi = {}", b, b_cubed, b, b_to_pi);
+
+    // bitwise
+    // | OR & AND ^ XOR ! NOR
+    let c = 1 | 2; // 01 OR 10 = 11 == 3_10
+    println!("1|2 = {}", c);
+
+    let two_to_10 = 1 << 10;
+    println!("2^10 = {}", two_to_10);
+
+    // logical
+    // < > <= >= ==
+    let pi_less_4 = std::f64::consts::PI < 4.0;
+
+    let x = 5;
+    let x_is_5 = x == 5;
+}
+
+fn main() {
+    // fundamental_data_types();
+    operators();
 }
