a6d6a70b4ae044f8268225cf9fabaabd3484a492
Elias Norrby <elias.norrby@gmail.com>
2019-12-15 16:13:51 +0100

    L7: Core Data Types

--- RustSample/hello_world/src/main.rs
+++ RustSample/hello_world/src/main.rs
@@ -1,3 +1,41 @@
+use std::mem;
+
 fn main() {
-    println!("Hello, world!");
+    // unsigned
+    let a: u8 = 123;
+    println!("a = {}", a);
+
+    // mutable signed
+    let mut b: i8 = 0;
+    println!("b = {}", b);
+    b = 42;
+    println!("b = {}", b);
+
+    let mut c = 123456789;
+    println!("c = {}, size = {} bytes", c, mem::size_of_val(&c));
+    c = -1;
+    println!("c = {} after modification", c);
+
+    // i8 u8 i16 u16 i32 u32 i64 u64
+    let z: isize = 123;
+    let size_of_z = mem::size_of_val(&z);
+    println!(
+        "z = {}, takes up {} bytes, {}-bit os",
+        z,
+        size_of_z,
+        size_of_z * 8
+    );
+
+    let d = 'x';
+    println!("d = {}, size = {} bytes", d, mem::size_of_val(&d));
+
+    let e = 2.5; // double-recision, 8 bytes or 64 bits, f64
+    println!("e = {}, size = {} bytes", e, mem::size_of_val(&e));
+
+    // true false
+    let g = false;
+    println!("g = {}, size = {} bytes", g, mem::size_of_val(&g));
+
+    let f = 4 > 0;
+    println!("f = {}, size = {} bytes", f, mem::size_of_val(&f));
 }
